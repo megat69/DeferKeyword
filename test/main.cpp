22fd8e7014839468ecf3c2e75d5e90f448efc2c1
megat69 <>
2025-10-20 22:00:31 +0200

    Added more comments

--- test/main.cpp
+++ test/main.cpp
@@ -6,13 +6,17 @@
 #define print(x) std::cout << x << std::endl
 
 int main() {
+    // Initializes the deferrer
     defer_init;
-    defer(print("Hello World!"));
+    print("Hello World!");
+    defer(print("Goodbye World!"));  // Last ever string printed to the console by this program
 
+    // Shows the deletion at the end of the scope of a heap-allocated object
     int* testVar = new int(5);
     defer(delete testVar; print("Deleted testVar"));
     std::cout << "New int: " << *testVar << " at address " << testVar << std::endl;
 
+    // Shows the automatic closing at the end of the scope of a file
     std::ifstream textFile("test/test.txt");
     defer(textFile.close());
     if (textFile.is_open()) {
